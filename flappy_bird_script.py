094f0ca9bc87652910b7a9147f5866fd36d0a467
Matthew Berthoud <mwberthoud@wm.edu>
2024-03-18 18:45:50 -0400

    video 4 done, pipes and floor move thru, bird doesn't

--- flappy_bird_script.py
+++ flappy_bird_script.py
@@ -4,7 +4,9 @@ import time
 import os
 import random
 
-WIN_WIDTH = 600 # can tweak these later if we notice the screen doesn't fit well
+pygame.font.init()
+
+WIN_WIDTH = 500 # can tweak these later if we notice the screen doesn't fit well
 WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
@@ -16,6 +18,8 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png
 
 # after writing this, test to make sure there's not syntax errors
 # maybe test showing the images just in case
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 
 class Bird:
@@ -106,7 +110,7 @@ class Pipe: # we are considering each pipe to be a top and bottom
 
         self.top = 0
         self.bottom = 0
-        self.PIPE_TOP = pygame.transform.flip(PIPG_IMG, False, True)
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
         self.PIPE_BOTTOM = PIPE_IMG
 
         self.passed = False
@@ -177,16 +181,26 @@ class Base:
 
 
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730) # 70 away from the bottom
+    pipes = [Pipe(600)] # controls how close the pipes are
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
+    score = 0
     run = True
     while run:
         clock.tick(30)
@@ -195,9 +209,33 @@ def main():
                 run = False
                 # pygame.quit()
             
-        # bird.move() # will just fall very quickly for now
-            # need to implement clock to make this slower
-        draw_window(win, bird)
+        add_pipe = False
+        rem = [] # list of pipes to remove
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass 
+                # end game, do something
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+
+        for r in rem:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()
     quit()
