569219c6fe279def9f0c2392a0c6d904aa2a3e68
Matthew Berthoud <mwberthoud@wm.edu>
2024-03-18 21:10:28 -0400

    removed comments and red screen

--- flappy_bird_script.py
+++ flappy_bird_script.py
@@ -219,6 +219,7 @@ def main():
         rem = [] # list of pipes to remove
         for pipe in pipes:
             if pipe.collide(bird):
+                # collided with pipe, end game
                 pass 
                 # end game, do something
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
@@ -239,20 +240,14 @@ def main():
 
         if bird.y + bird.img.get_height() >= 730:
             # hit the ground
-            print("ground")
             run = False
         elif bird.y <= 0:
-            print("ceiling")
+            # hit the ceiling
             run = False
 
         base.move()
         draw_window(win, bird, pipes, base, score)
 
-    print("You lost with score", score)
-
-    win.fill("red")
-    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
     text = STAT_FONT.render("You lost.", 1, (255,255,255))
     win.blit(text, (WIN_WIDTH // 2 - text.get_width() // 2, WIN_HEIGHT // 2 - text.get_height() // 2))
     pygame.display.update()
